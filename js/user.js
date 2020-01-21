5b69b00ac32dc1656e3cf515a1ee9c0bf3a84188
Cschilli1994 <52953412+Cschilli1994@users.noreply.github.com>
2020-01-21 11:38:13 +0900

    minor changes + readme
    changed date format from 01 JAN 1990 to 01/01/1990. added some CSS see readme

--- js/user.js
+++ js/user.js
@@ -147,24 +147,24 @@ class User {
         }
     get formattedDate(){
         const year = this.birthday.slice(0,4);
-        const month = this.month(this.birthday.slice(5,7));
+        const month = this.birthday.slice(5,7);
         const day = this.birthday.slice(8);
-        return `${day} ${month} ${year}`
-    }
-    month(date){
-        switch(date){
-            case '01':return 'JAN';
-            case '02':return 'FEB';
-            case '03':return 'MAR';
-            case '04':return 'APR';
-            case '05':return 'MAY';
-            case '06':return 'JUN';
-            case '07':return 'JUL';
-            case '08':return 'AUG';
-            case '09':return 'SEPT';
-            case '10':return 'OCT';
-            case '11':return 'NOV';
-            case '12':return 'DEC';
-        }
+        return ` ${month}/${day}/${year}`;
     }
 }
+    // month(date){
+    //     switch(date){
+    //         case '01':return 'JAN';
+    //         case '02':return 'FEB';
+    //         case '03':return 'MAR';
+    //         case '04':return 'APR';
+    //         case '05':return 'MAY';
+    //         case '06':return 'JUN';
+    //         case '07':return 'JUL';
+    //         case '08':return 'AUG';
+    //         case '09':return 'SEPT';
+    //         case '10':return 'OCT';
+    //         case '11':return 'NOV';
+    //         case '12':return 'DEC';
+    //     }
+    // }
---
+++ readme.txt
@@ -0,0 +1,12 @@
+    Javascript based webpage that requests 12 random employees from an API. Then dynamically builds each employees profile and 
+modal cards using data from the response and appends them to the page. The modals are hidden until an employees card is 
+selected from the gallery. A search bar is included to filter out employees that dont match what is typed in the box. While 
+searching if the user selects an employee profile card the modal navigation buttons will only navigate through employees matched
+by the search.
+
+CSS changes from original:
+    Changed body background color to an off white-yellow
+    Changed profile cards background color on hover and border changes
+        more apparent indication of selection
+    Added a border for modal navigation buttons on hover
+    
